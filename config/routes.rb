08e308f670180981b8187e80f1c188d2f5698b8c
yuki-tadaide <tadaide0420@gmail.com>
2019-11-03 11:17:21 +0900

    edit chat-spce

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+=======
+  
+  root 'messages#index'
+  
+>>>>>>> Stashed changes
 end
